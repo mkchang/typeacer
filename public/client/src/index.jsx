804cfe780ab41b1c94f00949738d33311b75e3a7
mkchang <martinkchang@gmail.com>
2017-08-05 12:12:08 -0700

    Add CSS file and incorporate into webpack

--- public/client/src/index.jsx
+++ public/client/src/index.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App.jsx';
+import AppCSS from './App.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
